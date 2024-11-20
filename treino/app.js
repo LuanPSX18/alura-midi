80fa870f76df0ec705bf94e675bb636f3bbbd8d6
LuanPSX18 <luanplaystation@gmail.com>
2024-11-20 19:25:57 -0300

    primeiro commit

---
+++ treino/app.js
@@ -0,0 +1,6 @@
+document.querySelector('input[type=tel]');
+
+// Escreva o código em JavaScript capaz de selecionar o elemento HTML input do tipo telefone:
+
+
+// Para selecionar o elemento HTML input do tipo tel podemos utilizar o código document.querySelector(‘input[type=tel]’), porquê com essa linha, o JavaScript vai acessar o documento HTML (representado por document) e procurar pelo elemento input que tenha como atributo [type=tel], os colchetes fazem parte da sintaxe para seleção de atributos de um elemento HTML, funciona tanto para o CSS quanto para o Javascript.
